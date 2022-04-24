519652122d7ec02a3a991916f968ec8b58748668
Iriadred <104327278+Iriadred@users.noreply.github.com>
2022-04-24 23:52:48 +0200

    Add files via upload

---
+++ Ph.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UFT-8" />
+    <title>PortFolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <link href="css/main.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<?php 
+      	$servername='localhost';
+          $dbname='port';
+          $username='root';
+          $password='';
+        $email= strip_tags($_POST["email"]);
+        $nom= strip_tags($_POST["Nom"]);
+        $descr= strip_tags($_POST["descr"]);
+      
+          try{
+              $conn= new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+
+              $requete = $conn->prepare("INSERT INTO `contact`(`email`, `Nom`, `description`) VALUES (:email,:nom,:descr);");
+        
+		    $requete->bindValue(':email',$email , PDO::PARAM_STR);
+		    $requete->bindValue(':nom',$nom , PDO::PARAM_STR);
+        $requete->bindValue(':descr',$descr , PDO::PARAM_STR);
+
+		    $requete->execute();
+          }
+        catch(PDOException $e){
+            echo "erreur :".$e->getMessage();
+            echo " Le numéro de l'erreur est : ".$e->getCode();
+            die;
+        }
+        header('Location:index.php ');
+      ?>
+
+
+</body>
+</html>  
---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UFT-8" />
+    <title>PortFolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <link href="main.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Portfolio</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#Accueil">Moi </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#exp">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#projet">Projet</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="cgu.html">CGU</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      <br>
+      <br>
+      <table id="Accueil" >
+      <div>
+            <h1>Moi  </h1> <br>  <br>
+            <h2>Bonjour, Je me nomme Mathieu Daudin et je suis un étudiant en BTS SIO .</h2><br>
+            <h2>Depuis mon plus jeune âge je suis intéréser au domaine de l'informatique et du multimédia. Donc pour pouvoir en decouvrir plus sur se domaine je me suis orienter aprés le brevet dans une baccalauréat professionnel Systéme numérique OPTION ARED (Audiovisuels, réseau et équipement domestiques). Puis à la fin de mon Bac j'ai decider de postuler en BTS SIO car la programmation m'a toujours intéréser et donc je me suis diriger vers l'option SLAM.</h2><br>
+            
+            <h3><u>Ma Formation</u></h3><br>
+
+            <h2>Le BTS Services informatiques aux organisations est un diplôme d'une durée de 2ans , Ou l'on apprend les bases théoriques et pratique dans le domaine de l'informatique pour préparer au monde profesionnel a partir de 2 option spécifiques :</h2>
+            <div class="card-group">
+                <div class="container">
+                 <br> <h2>-L'option SLAM(Solutions logicielles et applications métiers) forme des futurs développeurs dans le domaine du logiciel et dans le domaine du développement web .</h2><br>
+                </div> 
+                <div class="container"> 
+                  <br><h2>-L'option SISR(Solutions d’infrastructure, systèmes et réseaux) forme des futurs administrateur réseaux, Il sera donc capable de modifier ou d'adapter les solutions d'infrastructure ainsi que la qualité de service des équipements du réseaux.</h2>
+                  <br></div>
+                  </div>
+              </div>    
+            <br><h3><u>Mes Compétence</u></h3><br>
+            
+            <h2>Lors de mon cursus scolaire j'ai pu expérimenter divers domaines d'expérience qui sont surtout concentré sur l'informatique et le multimédia </h2><br>
+            <h2>Premièrement lors de mon bac professionnel j'ai pu découvrir le domaine de l'éléctroménager et multimédia. Puis lors de ma première année de BTS j'ai appris divers langagues web comme le HTML, le PHP et le CSS , et des lanqueges logiciels commes C# </h2> 
+            <h2>J'ai aussi appris l'utilisation de base de donnée par MariaDB en utilisant le langague SQL.</h2>
+
+            <br><h3><u>Cybersécurité</u></h3><br>
+
+            <h2> De nos jours oû la majorité des entreprises ont un système informatique les cyber attaque augmente <a href="https://bigmedia.bpifrance.fr/news/les-cyberattaques-ont-ete-multipliees-par-4-en-2020#:~:text=Une%20explosion%20des%20attaques%20par%20des%20ran%C3%A7ongiciels&text=En%20seulement%20un%20an%2C%20elles,piratages%20de%20comptes%20(11%20%25)."> Bigmédia les cyberattaque on augmenter de 255% en 2020<a>.Pour contré cela il est important de maintenir ça veille informatique pour pouvoir observer les faille informatique et donc pouvoir mieux les contrés.</h2> 
+          </div><br>
+      </table>
+     <Table id="exp">
+         <div>
+           <h1>Veille Technologique</h1><br>
+           <h2>Comme le monde de l'informatique est en perpétuelles évulutions un bonne informaticien ce doit de ce tenir au courant de c'est dite évolution. Pour ce faire plusieurs personnes on créer des sites internets ou bien des chaîne youtube par exemple pour pouvoir simplifié cette recherche d'actualité, Comme exemple de sites internets qui peuvent nous aidée à réalisez cette veille technologique l'on peut cité par exemple en sites français <a href="https://www.lemondeinformatique.fr/">le monde informatique</a> ou bien l'on peut trouver en plus connue <a href="https://github.com/search?q=developpement">GitHub</a>.Mais l'on peut aussi des chaîne youtube qui sont faite pour pouvoir renseigner par des vidéos spécifiques comme par exemple <a href="https://www.youtube.com/c/TechSama?app=desktop">Tech_sama <a> ou bien <a href="https://www.youtube.com/c/grafikart/videos">Grafikart.fr<a>. Pour cette recherche de savoir l'on peut aussi utiliser des Agrégateur de flux RSS comme <a href="https://feedly.com/"> feedly<a> ou bien <a href="https://www.inoreader.com/language/french"> Inoreader<a> .</h2>
+
+        </div> 
+     </Table> 
+     <table id="projet">
+      <tr>
+        <td>
+          <h1>Projet</h1>	<br>
+
+          <h4>1er Projet:</h4>
+          <div class="card-group">
+          <div class="container">
+            <h2>Lors de ce premier projet j'ai du mettre en place un cloud en ligne avec next cloud et réalisez des schémas de cablage à l'aide de cisco packet tracer</h2><br>
+          
+            <ul>
+              <h2>Pour rélisez ce projet j'ai du faire preuve d'envie de savoir par le fait que j'ignorait le fonctionnement du systéme et que j'ai du apprendre vite</h2>
+              <li><h2>J'ai du aussi faire preuve de persévérence face au difficulté </h2></li>
+            </ul>
+          </div>
+          <div class="container">
+            <img src="next.png" alt="Projets 01" style="width:44%;" />
+            <img src="cisco.png" alt="Projets 01" style="width:50%;" />
+          </div>
+          </div>
+          <h4>2eme Projet:</h4>
+          <div class="card-group">
+          <div class="container">
+            <h2>Durant ce projet j'ai du créer un site internet avec bootstrap et le mettre accesible à distance  </h2><br>
+
+            <h2>J'ai du faire preuve d'ingéniosité lors de ce projet pour trouvé des solution</h2>
+            <h2>Pour rélisez ce projet j'ai du faire preuve d'envie de savoir par le fait que j'ignorait le fonctionnement du systéme et que j'ai du apprendre vite</h2>
+          </div>
+          <div class="container">
+            <img src="index.PNG" alt="Projets 01" style="width:60%;" />
+          </div>
+          </div>
+        </td>
+      </tr>  
+     </table>
+     <table id="contact">
+      <div class="container" >
+        <h1>Contact</h1>
+      <form action ="Ph.php" method="post">
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" required>
+        </div>
+        <div class="form-group">
+          <label>Nom/prenom:</label>
+          <textarea class="form-control" id="Nom" rows="1" placeholder="Nom/prenom" name="Nom" required></textarea>
+        </div>
+        <div class="form-group">
+          <label>Description!</label>
+          <textarea class="form-control" id="message" rows="3" placeholder="descr" name="descr" required ></textarea>
+        </div>
+        <br><button type="submit" class="btn btn-light mb-2">Confirm identity</button>
+      </form>
+      </div>
+     </table>
+</body>
+</html>
